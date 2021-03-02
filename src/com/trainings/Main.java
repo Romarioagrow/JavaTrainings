df0f8e6f2154f9eb0e671e6412ab40e2a2fa7037
romarioagrow <romarioagrow@mail.ru>
2021-03-02 21:16:33 +0200

    isPalindrome

--- src/com/trainings/Main.java
+++ src/com/trainings/Main.java
@@ -1,10 +1,12 @@
 package com.trainings;
 
+import javax.xml.stream.events.Characters;
 import java.util.*;
-import java.util.concurrent.atomic.AtomicInteger;
-import java.util.stream.Collectors;
+import java.util.logging.*;
 
 public class Main {
+
+    private static final Logger LOGGER = Logger.getLogger("InfoLogging");
 
     public static final List<Integer> intInput = Arrays.asList(1, 1, 3, 3, 3, 4, 4);
 
@@ -13,25 +15,103 @@ public class Main {
     public static final int incomeNum = new Random().nextInt(50000);
 
     public static void main(String[] args) {
+        LOGGER.info("MAIN");
 
 
-        longestPrefix();
+        System.out.println(isPalindrome("Do geese see God?"));
 
-       // System.out.println(sumDigitsFromString("3242356"));
+    }
+
+
+    public static boolean isPalindrome(String s) {
+/// need memory O(1)
+/// memory O(n)       return s.equals(new StringBuilder(s.replaceAll("\\s", "").toLowerCase()).reverse().toString());
+        if (s == null || s.isEmpty()) return false;
+
+        int startIndex = 0, endIndex = s.length() - 1;
+
+        while (endIndex > startIndex) {
+
+            Character charLetterEnd = Character.toLowerCase(s.charAt(endIndex));
+            char charLetterStart = Character.toLowerCase(s.charAt(startIndex));
+
+            if (!Character.isLetter(charLetterEnd) && !Character.isLetter(charLetterStart) || charLetterEnd.equals(charLetterStart)) {
+                startIndex++;
+                endIndex--;
+            }
+            else if (!Character.isLetter(charLetterEnd)) {
+                endIndex--;
+            }
+            else if (!Character.isLetter(charLetterStart)) {
+                startIndex++;
+            }
+            else return false;
+        }
+
+        return true;
+ }
+
+    public static boolean checkValidBrackets(String inputString) {
+        if (inputString == null || inputString.isEmpty()) return false;
+
+        int figureBracketOpen = 0, figureBracketClose = 0, roundBracketOpen = 0, roundBacketClose = 0;
+
+        ArrayList<String> listString = new ArrayList<>(Arrays.asList(inputString.split("")));
+
+        for (String string : listString) {
+            switch (string) {
+                case "{" -> figureBracketOpen++;
+                case "}" -> figureBracketClose++;
+                case "(" -> roundBracketOpen++;
+                case ")" -> roundBacketClose++;
+            }
+        }
+        return figureBracketOpen == figureBracketClose && roundBracketOpen == roundBacketClose;
+    }
+
+
+    public static void trySetsAndLists() {
+
+
+        String[] arrayString = new String[]{"keke", "kok", "kokos", "axaxaxaxa"};
+
+        List<String> list = new ArrayList<>(Arrays.asList(arrayString));
+        Set<String> set = new HashSet<>(Set.of(arrayString));
+
+
+        System.out.format("\nlist %s", list.toString());
+        System.out.format("\nset %s", set.toString());
+
+        //.set. set.
+
+        list.sort(Comparator.comparing(String::length));
+
+
+        System.out.format("\nlist after sort %s", list.toString());
 
 
     }
 
 
+    public static void calculateSqrt(int input) {
+
+        int sqrt = (int) Math.sqrt(input);
+
+        System.out.println(sqrt);
+
+
+    }
+
     public static void longestPrefix() {
-        final String[] strings = new String[] {"abc", "abcegeg", "abcde", "abcg"};
+        final String[] strings = new String[]{"abc", "abcegeg", "abcde", "abcg"};
         /**/
 
         String prefix = "";
         Arrays.sort(strings, Comparator.comparingInt(String::length));
         String firstWord = strings[0];
 
-        letters: for (int i = 0; i < firstWord.length(); i++) {
+        letters:
+        for (int i = 0; i < firstWord.length(); i++) {
             String checkLetter = String.valueOf(firstWord.charAt(i));
 
             for (int j = 1; j < strings.length; j++) {
@@ -47,12 +127,11 @@ public class Main {
 
 
     public static int sumDigitsFromString(String digit) {
-        try{
+        try {
             return Arrays.stream(digit.split(""))
                     .map(Integer::parseInt)
                     .reduce(0, Integer::sum);
-        }
-        catch (NumberFormatException e) {
+        } catch (NumberFormatException e) {
             return 0;
         }
 
